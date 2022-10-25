8a5616f8f77d5963807e670c6cff99d89e6f6ca3
andre-alck <andrealck1@gmail.com>
2022-10-25 16:55:51 -0300

    feat: retrieve and update dogs

--- lib/db_test.dart
+++ lib/db_test.dart
@@ -15,7 +15,7 @@ void main() async {
       version,
     ) {
       return db.execute(
-        'CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, age INTEGER',
+        'CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)',
       );
     },
     version: 1,
@@ -32,13 +32,65 @@ void main() async {
     );
   }
 
-  const bidu = Dog(
+  Future<List<Dog>> dogs() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query(
+      'dogs',
+    );
+
+    return List.generate(
+      maps.length,
+      (
+        index,
+      ) {
+        return Dog(
+          id: maps[index]['id'],
+          name: maps[index]['name'],
+          age: maps[index]['age'],
+        );
+      },
+    );
+  }
+
+  Future<void> updateDog(
+    Dog dog,
+  ) async {
+    final db = await database;
+    await db.update(
+      'dogs',
+      dog.toMap(),
+      where: 'id = ?',
+      whereArgs: [
+        dog.id,
+      ],
+    );
+  }
+
+  var bidu = const Dog(
     id: 1,
     name: 'Bidu',
     age: 7,
   );
 
   await insertDog(
+    bidu,
+  );
+
+  print(
+    await dogs(),
+  );
+
+  bidu = Dog(
+    id: bidu.id + 1,
+    name: bidu.name,
+    age: bidu.age,
+  );
+
+  await updateDog(
+    bidu,
+  );
+
+  print(
     bidu,
   );
 }
