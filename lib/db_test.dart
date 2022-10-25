f0248895c71f88813b3a4062d17b2a11c3fd92d3
andre-alck <andrealck1@gmail.com>
2022-10-25 15:27:05 -0300

    refactor: update to documentation

--- lib/db_test.dart
+++ lib/db_test.dart
@@ -12,3 +12,15 @@ void main() async {
     ),
   );
 }
+
+class Dog {
+  final int id;
+  final String name;
+  final int age;
+
+  const Dog({
+    required this.id,
+    required this.name,
+    required this.age,
+  });
+}
